d6c2f8d384c7e5f10ed3fad5693f8ad25e39d2c5
Nick Nguyen <55971813+ninhdangcap123@users.noreply.github.com>
2024-10-25 12:31:19 +0700

    UPdate tterraform

--- deployment/development/terraform/main.tf
+++ deployment/development/terraform/main.tf
@@ -225,21 +225,20 @@ resource "aws_ecr_repository" "my_ecr" {
   }
 }
 
-# IAM role for EKS
+# IAM Role for EKS Cluster
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks_cluster_role"
+  name = "ninhnh-vti-eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action    = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "eks.amazonaws.com"
         }
-        Effect    = "Allow"
-        Sid       = ""
-      },
+        Action = "sts:AssumeRole"
+      }
     ]
   })
 
@@ -248,7 +247,13 @@ resource "aws_iam_role" "eks_cluster_role" {
   }
 }
 
-# Create an EKS cluster
+# Attach policies to the EKS Cluster IAM role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# Create an EKS Cluster
 resource "aws_eks_cluster" "my_cluster" {
   name     = "ninhnh-vti-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -262,9 +267,9 @@ resource "aws_eks_cluster" "my_cluster" {
   }
 }
 
-# IAM Role for EKS Nodes (Node Group)
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks_node_role"
+# IAM Role for EKS Node Group
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "ninhnh-vti-eks-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -275,7 +280,7 @@ resource "aws_iam_role" "eks_node_role" {
           Service = "ec2.amazonaws.com"
         }
         Action = "sts:AssumeRole"
-      },
+      }
     ]
   })
 
@@ -284,40 +289,49 @@ resource "aws_iam_role" "eks_node_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+# Attach necessary policies to the IAM role for the node group
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+resource "aws_iam_role_policy_attachment" "cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+resource "aws_iam_role_policy_attachment" "ec2_registry_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
-# EKS Node Group
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "ninhnh-vti-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.private_subnet[*].id
+# # EKS Node Group
+# resource "aws_eks_node_group" "eks_node_group" {
+#   cluster_name    = aws_eks_cluster.my_cluster.name
+#   node_group_name = "ninhnh-vti-node-group"
+#   node_role_arn   = aws_iam_role.eks_node_group_role.arn
+#   subnet_ids      = aws_subnet.private_subnet[*].id
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 2
+#     max_size     = 3
+#     min_size     = 1
+#   }
 
-  instance_types = ["t3.medium"]
+#   instance_types = ["t3.medium"]
 
-  tags = {
-    Name = "ninhnh-vti-eks-node-group"
-  }
-}
+#   tags = {
+#     Name = "ninhnh-vti-eks-node-group"
+#     "kubernetes.io/cluster/${aws_eks_cluster.my_cluster.name}" = "shared"  # Ensure tagging
+#   }
+
+#   depends_on = [
+#     aws_iam_role.eks_node_group_role,  # Ensure IAM role is created before the node group
+#     aws_iam_role_policy_attachment.worker_node_policy,
+#     aws_iam_role_policy_attachment.cni_policy,
+#     aws_iam_role_policy_attachment.ec2_registry_policy
+#   ]
+# }
 
 # IAM Role for EC2 (Jenkins)
 resource "aws_iam_role" "jenkins_role" {
@@ -418,11 +432,6 @@ resource "aws_instance" "jenkins" {
 # Output the EKS cluster endpoint
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.my_cluster.endpoint
-}
-
-# Output the EKS cluster name
-output "eks_cluster_name" {
-  value = aws_eks_cluster.my_cluster.name
 }
 
 # Output the ECR repository URL
